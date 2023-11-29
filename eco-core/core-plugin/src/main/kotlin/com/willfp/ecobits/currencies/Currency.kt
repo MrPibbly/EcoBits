040b991c41dacba11ee6ea84dfaf6245a38d84f4
MrPibbly <109881084+MrPibbly@users.noreply.github.com>
2023-11-29 01:01:14 -0600

    Update Currency.kt
    Idk i hope this works lel

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/currencies/Currency.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecobits/currencies/Currency.kt
@@ -135,7 +135,7 @@ class Currency(
                 plugin,
                 "${id}_raw"
             ) {
-                it.getBalance(this).toNiceString()
+                it.getBalance(this).toString()
             }
         )
 
